e27afd62c3c6b99b9d5683cf30953e93ba9d485d
Elias Sundqvist <elias@sundqvist.com>
2019-04-10 19:19:46 +0200

    Fix colors and make dummy button less visible

--- app/presenter/widget/navbar.py
+++ app/presenter/widget/navbar.py
@@ -6,28 +6,34 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivymd.button import MDFloatingActionButton
 from kivymd.tabs import MDBottomNavigation, MDBottomNavigationItem
-from kivymd.theming import ThemeManager
+from kivymd.theming import ThemeManager, ThemableBehavior
 
 
-class NavBar(BoxLayout):
+class NavBar(BoxLayout, ThemableBehavior):
     tabs = ListProperty()
     fab_icon = StringProperty('android')
     fab_callback = ObjectProperty(lambda x: None)
-    fab_color = ListProperty([1, 0, 0])
+    fab_color = ListProperty([1, .7568627450980392, .027450980392156862, 1])
 
     def __init__(self, **kwargs):
         super(NavBar, self).__init__(**kwargs)
 
         float_layout = FloatLayout()
         self.nav_bar = MDBottomNavigation()
-        for tab in self.tabs:
-            self.add_tab(**tab)
 
-        self.fab = AppBarActionButton(y=self.nav_bar.height // 2,
-                                      x=Window.width // 2 - dp(56) // 2, opacity=1, size=(dp(56), dp(56)),
-                                      on_release=self.fab_callback,
-                                      action_button_color=self.fab_color,
-                                      icon=self.fab_icon)
+        for tab in self.tabs:
+            if tab is None:
+                self.add_space()
+            else:
+                self.add_tab(**tab)
+
+        self.fab = MDFloatingActionButton(y=self.nav_bar.height // 2,
+                                          x=Window.width // 2 - dp(56) // 2, opacity=1, size=(dp(56), dp(56)),
+                                          on_release=self.fab_callback,
+                                          icon=self.fab_icon)
+
+        self.fab.md_bg_color = self.fab_color
+        self.fab.elevation_normal = 8
 
         float_layout.add_widget(self.nav_bar)
         float_layout.add_widget(self.fab)
@@ -47,6 +53,26 @@ class NavBar(BoxLayout):
         nav_item.add_widget(widget)
         self.nav_bar.add_widget(nav_item)
 
+    def add_space(self):
+        self.nav_bar.add_widget(DummyNavItem())
 
-class AppBarActionButton(MDFloatingActionButton):
-    action_button_color = ListProperty()
+
+class DummyNavItem(MDBottomNavigationItem):
+    def __init__(self):
+        self.icon='dots-horizontal'
+        pass
+
+    def on_tab_touch_down(self, *args):
+        pass
+
+    def on_tab_touch_move(self, *args):
+        pass
+
+    def on_tab_touch_up(self, *args):
+        pass
+
+    def on_tab_press(self, *args):
+        pass
+
+    def on_tab_release(self, *args):
+        pass
