3b4f6d970534de3c5f00697ff5f9d78f8fe0c925
pilino1234 <pilino1234@zoho.eu>
2019-04-15 19:11:27 +0200

    Travis: Enable pydocstyle in Prospector and add docstrings everywhere
    This enforces documentation coverage.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+"""The entry point of the Carrepsa app"""
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.theming import ThemeManager
@@ -6,9 +8,20 @@ from presenter.navbar import NavBarWithFAB
 
 
 class Carrepsa(App):
+    """
+    The main class of the Carrepsa app
+
+    This class handles the initial construction of the interface.
+    """
+
     theme_cls = ThemeManager(primary_palette="Blue")
 
-    def build(self):
+    def build(self) -> BoxLayout:
+        """
+        Builds the root view of the application window
+
+        :return: The root BoxLayout of the window
+        """
         root = BoxLayout()
         root.add_widget(
             NavBarWithFAB(fab_callback=lambda: print("FAB pressed!")))
