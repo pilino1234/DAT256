de78370a0aa9c35cb5e5bd7ce0553e8c0c1ff355
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2019-04-16 16:04:29 +0200

    Code cleanup

--- app/presenter/delivery_list.py
+++ app/presenter/delivery_list.py
@@ -1,59 +1,68 @@
 from kivy.lang import Builder
-from kivy.properties import NumericProperty, OptionProperty
+from kivy.properties import NumericProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivymd.elevationbehavior import RectangularElevationBehavior
-from kivymd.button import MDIconButton, MDRoundFlatButton, MDRaisedButton
-from kivymd.list import ILeftBody, ILeftBodyTouch, IRightBodyTouch, OneLineIconListItem
+from kivymd.button import MDIconButton, MDRaisedButton
+from kivymd.list import ILeftBodyTouch, OneLineIconListItem
 from kivy.metrics import dp
-
 
 Builder.load_file("view/delivery_list.kv")
 
 
-class GoodButton(MDRaisedButton):
-    _radius = NumericProperty(dp(18))
-
-    def lay_canvas_instructions(self):
-        with self.canvas.after:
-            StencilPush()
-            RoundedRectangle(size=self.size,
-                             pos=self.pos,
-                             radius=[self._radius, ])
-            StencilUse()
-            self.col_instruction = Color(rgba=self.ripple_color)
-            self.ellipse =\
-                Ellipse(size=(self.ripple_rad, self.ripple_rad),
-                        pos=(self.ripple_pos[0] - self.ripple_rad / 2.,
-                             self.ripple_pos[1] - self.ripple_rad / 2.))
-            StencilUnUse()
-            RoundedRectangle(size=self.size,
-                             pos=self.pos,
-                             radius=[self._radius, ])
-            StencilPop()
-        self.bind(ripple_color=self._set_color, ripple_rad=self._set_ellipse)
-
 class DeliveryList(BoxLayout):
+    """
+    Widget that lists all available delivery requests.
+
+    Each request is represented with a ListItem.
+    """
+
     pass
 
-class ListItem(GoodButton):
-    _radius = NumericProperty(14)
+
+class WhiteCardButton(MDRaisedButton):
+    """Widget that alters MDRaisedButton to a blank card-looking button with a drop shadow."""
+
+    _radius = NumericProperty(dp(14))
+
+
+class ListItem(WhiteCardButton):
+    """Widget that represents all the content of a list item."""
+
     pass
 
-class IconLeftSampleWidget(ILeftBodyTouch, MDIconButton):
-    pass
+
+class HollowIcon(ILeftBodyTouch, MDIconButton):
+    """
+    An icon that can't be interacted with.
+
+    Events are instead passed through to the underlying widget.
+    """
+
+    def on_release(self, *args):  # noqa: D102
+        pass
+
+    def on_touch_down(self, *args):  # noqa: D102
+        pass
+
+    def on_touch_move(self, *args):  # noqa: D102
+        pass
+
+    def on_touch_up(self, *args):  # noqa: D102
+        pass
+
 
 class IconWithText(OneLineIconListItem):
+    """A left-aligned icon with some text to the right. Non-interactable."""
+
     divider = None
 
-    def on_release(self, *args):
+    def on_release(self, *args):  # noqa: D102
         pass
 
-    def on_touch_down(self, *args):
+    def on_touch_down(self, *args):  # noqa: D102
         pass
 
-    def on_touch_move(self, *args):
+    def on_touch_move(self, *args):  # noqa: D102
         pass
 
-    def on_touch_up(self, *args):
+    def on_touch_up(self, *args):  # noqa: D102
         pass
-
