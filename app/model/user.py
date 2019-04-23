d42cdc6460d82d8c275a7f791e08acb4c75df93a
OlleLindgren <lindgrenolle@live.se>
2019-04-23 12:16:39 +0200

    User profile view getting data from model
    presenter has a user object and view gets data from that object via the presenter.

---
+++ app/model/user.py
@@ -0,0 +1,40 @@
+class User:
+
+    def __init__(self, name, email, phone, avatar):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.avatar = avatar
+
+    def replace_all(self, name, email, phone, avatar):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.avatar = avatar
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_email(self):
+        return self.email
+
+    def set_email(self, email):
+        self.email = email
+
+    def get_phone(self):
+        return self.phone
+
+    def set_phone(self, phone):
+        self.phone = phone
+
+    def get_avatar(self):
+        return self.avatar
+
+    def set_avatar(self, avatar):
+        self.avatar = avatar
+
+
+
--- app/presenter/user_profile_view.py
+++ app/presenter/user_profile_view.py
@@ -2,8 +2,15 @@ from kivy.lang import Builder
 
 from kivy.uix.boxlayout import BoxLayout
 
+from model.user import User
+
 Builder.load_file("view/user_profile_view.kv")
 
 
 class UserProfileView(BoxLayout):
-    pass
+    user_me = User(
+        "Name Surname",
+        "asdf@example.com",
+        "0706123123",
+        "https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg"
+    )
