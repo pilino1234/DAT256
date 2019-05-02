cfc7c27c88e8ec58de85837c4416a99dde61e2f3
Kevin <kevinkullgren@hotmail.com>
2019-05-02 11:17:03 +0200

    Add initial delivery request commit
    Nytt

---
+++ app/presenter/deliveryrequest.py
@@ -0,0 +1,10 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+
+Builder.load_file("view/deliveryrequest.kv")
+
+
+class DeliveryRequest(BoxLayout):
+    """Widget to create a deliveryrequest"""
+
+    pass
