f5f7f498c957875c59e31975c6ef20ea30d88a00
pilino1234 <pilino1234@zoho.eu>
2019-05-05 21:34:26 +0200

    Use an enum for package status

--- app/model/delivery_request.py
+++ app/model/delivery_request.py
@@ -1,4 +1,13 @@
 from collections import namedtuple
+from enum import IntEnum
+
+
+class Status(IntEnum):
+    """Status codes for delivery requests."""  # noqa: D204
+    AVAILABLE = 0
+    ACCEPTED = 1
+    TRAVELLING = 2
+    DELIVERED = 3
 
 
 class DeliveryRequest:
@@ -12,11 +21,9 @@ class DeliveryRequest:
         _weight_prop('truck', "Huge")
     ]
 
-    _status_types = ["available", "accepted", "travelling", "delivered"]
-
     def __init__(self, item: str, description: str, origin: str,
                  destination: str, reward: int, weight: int, fragile: bool,
-                 status: int, money_lock: int):
+                 status: Status, money_lock: int):
         """Initializes the delivery list"""
         self.item = item
         self.description = description
@@ -30,7 +37,7 @@ class DeliveryRequest:
 
         self.weight_text = self._weight_props[weight].text
         self.weight_icon = self._weight_props[weight].icon
-        self.status_text = self._status_types[status]
+        self.status_text = self.status.name.title()
 
     def get_distance_pretty(self) -> str:
         """Computes distance between origin and destination in kilo meters"""
