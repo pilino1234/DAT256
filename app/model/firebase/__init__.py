dfdadc44b777dd3e496531f79e6eec810da75b05
Theodor Angergård <portalprogramming@gmail.com>
2019-05-06 14:30:58 +0200

    uses rane instead of six.xrange

--- app/model/firebase/__init__.py
+++ app/model/firebase/__init__.py
@@ -4,19 +4,23 @@ import os
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "keyfile.json"
 
-
 class Firebase:
     """Stores the common firebase db and storage bucket instance."""
-
-    db = firestore.Client()
-    bucket = storage.Client().get_bucket("carrepsa.appspot.com")
 
     @staticmethod
     def get_db():
         """Fetch the common firebase db instance."""
-        return Firebase.db
+
+        if not hasattr(Firebase, "_db"):
+            Firebase._db = firestore.Client()
+
+        return Firebase._db
 
     @staticmethod
     def get_bucket() -> storage.Bucket:
         """Fetch the common firebase storage bucket instance"""
-        return Firebase.bucket
+
+        if not hasattr(Firebase, "_bucket"):
+            Firebase._bucket = storage.Client().get_bucket("carrepsa.appspot.com")
+
+        return Firebase._bucket
