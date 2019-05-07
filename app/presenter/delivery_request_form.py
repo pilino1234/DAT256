93049bf3bb5f25492b0f578bd40ebbc125f42046
pilino1234 <pilino1234@zoho.eu>
2019-05-07 19:53:34 +0200

    Implement submitting and uploading delivery requests

--- app/presenter/delivery_request_form.py
+++ app/presenter/delivery_request_form.py
@@ -1,6 +1,10 @@
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.button import MDIconButton
+from kivymd.textfields import MDTextField
+
+from model.delivery_request import DeliveryRequest, Status
+from model.delivery_request_uploader import DeliveryRequestUploader
 
 Builder.load_file("view/delivery_request.kv")
 
@@ -15,9 +19,15 @@ class DeliveryRequestForm(BoxLayout):
         'weight_truck_button'
     ]
 
+    __text_field_ids = [
+        'package_name', 'description_text', 'from_text', 'dest_text',
+        'payment_amount', 'money_lock_amount'
+    ]
+
     def __init__(self, **kwargs):
         """Instantiate a form for submitting a delivery request."""
         super(DeliveryRequestForm, self).__init__(**kwargs)
+
         for button_id in self._weights:
             button: MDIconButton = self.ids[button_id]
 
@@ -25,6 +35,10 @@ class DeliveryRequestForm(BoxLayout):
                 on_press=lambda _, bid=button_id: self._set_weight(bid))
 
         self._set_weight(self._weights[self.weight])
+
+        for text_id in self.__text_field_ids:
+            text_field: MDTextField = self.ids[text_id]
+            text_field.bind(text=self._verify_entries)
 
     def _set_weight_cb(self, button_id: str, _button: MDIconButton):
         self._set_weight(button_id)
@@ -43,3 +57,61 @@ class DeliveryRequestForm(BoxLayout):
     def __highlight_weight_button(self, button_id: str):
         button: MDIconButton = self.ids[button_id]
         button.text_color = [50 / 255, 205 / 255, 50 / 255, 1]
+
+    def _verify_entries(self, *_args) -> bool:
+        """
+        Verify the entries in the request form.
+
+        :return: True if all inputs are valid
+                 False otherwise
+        """
+
+        all_good = (len(self.ids.package_name.text) > 0 and
+                    len(self.ids.description_text.text) > 0 and
+                    len(self.ids.from_text.text) > 0 and
+                    len(self.ids.dest_text.text) > 0 and
+                    len(self.ids.payment_amount.text) > 0 and
+                    int(self.ids.payment_amount.text) >= 0 and
+                    len(self.ids.money_lock_amount.text) > 0 and
+                    int(self.ids.money_lock_amount.text))
+
+        self.ids.request_button.disabled = not all_good
+        return all_good
+
+    def _clear_form(self):
+        self.ids.package_name.text = ""
+        self.ids.from_text.text = ""
+        self.ids.dest_text.text = ""
+        self.ids.payment_amount.text = ""
+        self.ids.money_lock_amount.text = ""
+        self.ids.description_text.text = ""
+        self._set_weight(self._weights[2])
+        self.ids.fragile_bool.active = False
+
+    def _submit_request(self):
+        payment_amount: int = int(self.ids.payment_amount.text)
+
+        # TODO: implement this once user "management" is in place
+#        if payment_amount > user.account_balance:
+#            self.display_warning("You don't have enough money to pay for this delivery")
+#            return
+#        else:
+#            user.lock_delivery_amount(payment_amount)
+
+        request = DeliveryRequest(item=self.ids.package_name.text,
+                                  description=self.ids.description_text.text,
+                                  origin=self.ids.from_text.text,
+                                  destination=self.ids.dest_text.text,
+                                  reward=payment_amount,
+                                  weight=self.weight,
+                                  fragile=self.ids.fragile_bool.active,
+                                  status=Status.AVAILABLE,
+                                  money_lock=int(self.ids.money_lock_amount.text))
+
+        DeliveryRequestUploader.upload(request)
+
+        # Hide the sliding popup. We are placed in a SlidingPopupContent widget,
+        # whose parent is the SlidingPopup we need.
+        self.parent.parent.hide()
+
+        self._clear_form()
