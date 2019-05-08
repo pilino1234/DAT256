fbe235560e51d3ded6dc9b72b65796d70e3ff03a
pilino1234 <pilino1234@zoho.eu>
2019-05-08 23:38:44 +0200

    Travis: install project deps before running tests
    Firebase: Refactor so that we can actually run unit tests

--- ci/run_nose.sh
+++ ci/run_nose.sh
@@ -1,6 +1,26 @@
 
 pip --quiet install nose2 codecov
 
+# Kivy deps
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential \
+    ffmpeg \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-ttf-dev \
+    libportmidi-dev \
+    libswscale-dev \
+    libavformat-dev \
+    libavcodec-dev \
+    zlib1g-dev \
+    libgl1-mesa-dev
+
+# Need to install Cython before Kivy
+pip install cython==0.28.6
+pip install -r app/requirements.txt
+
 nose2 --start-dir app/ --with-coverage
 codecov --token=$CODECOV_TOKEN
 
