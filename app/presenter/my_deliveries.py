4497645453a9d3a1ecf4a626095193147fb0f9ab
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2019-05-08 14:37:18 +0200

    Subscribe to packages

--- app/presenter/my_deliveries.py
+++ app/presenter/my_deliveries.py
@@ -8,14 +8,6 @@ from model.firebase.firestore import Firestore
 
 Builder.load_file("view/my_deliveries.kv")
 
-_delivery_requests = []
-
-docs = Firestore.get_raw('packages').where(u'assistant', u'==', u'pIAeLAvHXp0KZKWDzTMz').get()
-for doc in docs:
-    data = doc.to_dict()
-    data['status'] = Status(data.get('status'))
-    _delivery_requests.append(DeliveryRequest(**data))
-
 class MyDeliveries(BoxLayout):
     """
     Widget that lists all delivery requests accepted by the user.
@@ -26,9 +18,20 @@ class MyDeliveries(BoxLayout):
     def __init__(self, **kwargs):
         """Initializes the delivery list"""
         super(MyDeliveries, self).__init__(**kwargs)
-        Clock.schedule_once(self._init_content)
+        Clock.schedule_once(lambda dt: self._update_content())
+        Firestore.subscribe("packages", lambda a, b, c: self._update_content())
 
-    def _init_content(self, delta_time):
+    def _update_content(self):
+        """Fech my deliveries I have accepted"""
+        docs = Firestore.get_raw('packages').where(u'assistant', u'==', u'pIAeLAvHXp0KZKWDzTMz').get()
+
+        delivery_requests = []
+        for doc in docs:
+            data = doc.to_dict()
+            data['status'] = Status(data.get('status'))
+            delivery_requests.append(DeliveryRequest(**data))
+
         """Fill delivery list"""
-        for req in _delivery_requests:
+        self.ids.my_deliveries.clear_widgets()
+        for req in delivery_requests:
             self.ids.my_deliveries.add_widget(ListItem(req))
