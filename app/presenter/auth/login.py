a7f4265bf21987e40ea5cb80c51be816bb6dc0f1
Elias Sundqvist <elias@sundqvist.com>
2019-05-10 19:07:23 +0200

    Add Router Widget and basic authentication views

---
+++ app/presenter/auth/login.py
@@ -0,0 +1,10 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+
+Builder.load_file("view/auth/login.kv")
+
+
+class Login(BoxLayout):
+    """The login view"""
+
+    pass
