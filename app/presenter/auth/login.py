448efdc69923b69170d58985f4e0c9b20669445b
Theodor Angergård <portalprogramming@gmail.com>
2019-05-13 16:06:40 +0200

    You can now sign up

--- app/presenter/auth/login.py
+++ app/presenter/auth/login.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 
-from model.firebase import Firebase
+from model.firebase.auth import Auth
 
 Builder.load_file("view/auth/login.kv")
 
@@ -13,7 +13,7 @@ class Login(BoxLayout):
     def authenticate(self):
         email = self.ids.email_tf.text
         password = self.ids.password_tf.text
-        if(Firebase.sign_in(email, password)):
+        if(Auth.sign_in(email, password)):
             app = App.get_running_app()
             app.is_authenticated = True
         else:
