9d6fa57d28025c822334ce7391f19433761a126d
Theodor Angergård <portalprogramming@gmail.com>
2019-05-13 08:59:27 +0200

    Starts work with register

--- app/model/firebase/__init__.py
+++ app/model/firebase/__init__.py
@@ -6,6 +6,8 @@ from google.auth import _helpers
 
 from kivy.app import App
 
+from model.firebase.firestore import Firestore
+
 webApiKey = "AIzaSyAZyHcBO03Pf9RK0eM3ifYErGG8eP57aTA"  # Web Api Key
 
 from kivy.storage.jsonstore import JsonStore
@@ -48,6 +50,32 @@ class Firebase:
             return True
 
         return False
+
+    @staticmethod
+    def sign_up(email, password, name, phonenumber):
+        signup_url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + webApiKey
+        signup_payload = {"email": email, "password": password, "returnSecureToken": True}
+        sign_up_request = requests.post(signup_url, data=signup_payload)
+        sign_up_data = json.loads(sign_up_request.content.decode())
+
+        if sign_up_request.ok == True:
+            token = sign_up_data['idToken']
+            refresh_token = sign_up_data['refreshToken']
+            uid = sign_up_data['localId']
+
+            Firebase.sign_in_with_tokens(token, refresh_token)
+            # with Firestore.batch("/users") as batch:
+            #     pass
+                # batch.set(uid, {"email": email, "name": name, "phonenumber": phonenumber})
+
+        elif sign_up_request.ok == False:
+            error_data = json.loads(sign_up_request.content.decode())
+            error_message = error_data["error"]['message']
+            print(error_data)
+            print(error_message)
+            if error_message == "EMAIL_EXISTS":
+                print("finns redan denna mail")
+
 
     @staticmethod
     def get_db() -> fs.Client:
--- app/presenter/auth/register.py
+++ app/presenter/auth/register.py
@@ -1,10 +1,20 @@
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 
+from model.firebase import Firebase
+
 Builder.load_file("view/auth/register.kv")
 
 
 class Register(BoxLayout):
     """The registration view"""
 
-    pass
+    def register(self):
+        email = self.ids.email_tf.text
+        password = self.ids.password_tf.text
+        password_repeat = self.ids.password_repeat_tf.text
+        name = self.ids.name_tf.text
+        phonenumber = self.ids.phonenumber_tf.text
+
+        if(password == password_repeat):
+            Firebase.sign_up(email, password, name, phonenumber)
