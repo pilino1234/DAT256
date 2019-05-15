110f3a9ffd2b38b45bd4b25d87be8b5e426bbf3f
Theodor Angergård <portalprogramming@gmail.com>
2019-05-15 22:54:24 +0200

    Moves FirebaseCredentials from auth to its own file

---
+++ app/model/firebase/firebase_credentials.py
@@ -0,0 +1,27 @@
+from google.auth.credentials import Credentials
+from google.auth import _helpers
+
+class FirebaseCredentials(Credentials):
+    """Firebase Credentials, stores the authentication tokens"""
+    token = None
+    refresh_token = None
+
+    def __init__(self, token, refresh_token):
+        super().__init__()
+        self.token = token
+        self.refresh_token = refresh_token
+
+    def apply(self, headers, token=None):
+        """Apply the token to the authentication header.
+
+        Args:
+            headers (Mapping): The HTTP request headers.
+            token (Optional[str]): If specified, overrides the current access
+                token.
+        """
+
+        headers['authorization'] = 'Bearer {}'.format(
+            _helpers.from_bytes(self.token))
+
+    def refresh(self, request):
+        print("nah")
