2c2446314c6fb7bf3c6dd868c6b5aa06ddbc9045
Theodor Angergård <portalprogramming@gmail.com>
2019-05-15 20:20:31 +0200

    Feat/storage images (#70)
    * Adds code for bucket
    
    * Adds upload_raw
    
    * Resolves yapf and prospector issues
    
    * Adds google-cloud-storage as requirement
    
    * Changes from hasattr to simple if case
    
    * Clarify doc strings

--- app/model/firebase/__init__.py
+++ app/model/firebase/__init__.py
@@ -1,19 +1,29 @@
-import os
+from google.cloud import firestore as fs, storage  # type: ignore
 
-from google.cloud.firestore_v1 import Client
+import os
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "keyfile.json"
 
 
 class Firebase:
-    """Stores the common firebase db instance."""
+    """Stores the common firebase db and storage bucket instance."""
 
-    _db: Client
+    _db = None
+    _bucket = None
 
     @staticmethod
-    def get_db() -> Client:
+    def get_db():
         """Fetch the common firebase db instance."""
-        if not hasattr(Firebase, '_db'):
-            Firebase._db = Client()
+        if Firebase._db is None:
+            Firebase._db = fs.Client()
 
         return Firebase._db
+
+    @staticmethod
+    def get_bucket() -> storage.Bucket:
+        """Fetch the common firebase storage bucket instance"""
+        if Firebase._bucket is None:
+            Firebase._bucket = storage.Client().get_bucket(
+                "carrepsa.appspot.com")
+
+        return Firebase._bucket
