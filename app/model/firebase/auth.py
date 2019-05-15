04a0090bd231ae0132d4c0621a09223b0d905818
Theodor Angergård <portalprogramming@gmail.com>
2019-05-15 22:54:24 +0200

    Moves FirebaseCredentials from auth to its own file

--- app/model/firebase/auth.py
+++ app/model/firebase/auth.py
@@ -80,28 +80,3 @@ class Auth:
             if error_message == "EMAIL_EXISTS":
                 print("finns redan denna mail")
 
-
-class FirebaseCredentials(Credentials):
-    """Firebase Credentials, stores the authentication tokens"""
-    token = None
-    refresh_token = None
-
-    def __init__(self, token, refresh_token):
-        super().__init__()
-        self.token = token
-        self.refresh_token = refresh_token
-
-    def apply(self, headers, token=None):
-        """Apply the token to the authentication header.
-
-        Args:
-            headers (Mapping): The HTTP request headers.
-            token (Optional[str]): If specified, overrides the current access
-                token.
-        """
-
-        headers['authorization'] = 'Bearer {}'.format(
-            _helpers.from_bytes(self.token))
-
-    def refresh(self, request):
-        print("nah")
