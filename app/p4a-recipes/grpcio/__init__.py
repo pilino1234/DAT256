d4f2ffb01d58dff082622ef700e269f172c78f8c
pilino1234 <pilino1234@zoho.eu>
2019-05-16 14:45:09 +0200

    p4a recipe: Add setuptools to grpcio dependencies

--- app/p4a-recipes/grpcio/__init__.py
+++ app/p4a-recipes/grpcio/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 class GrpcioRecipe(CompiledComponentsPythonRecipe):
     version = '1.20.1'
     url = 'https://pypi.python.org/packages/source/g/grpcio/grpcio-{version}.tar.gz'
-    depends = []
+    depends = ['setuptools']
 
 
 recipe = GrpcioRecipe()
