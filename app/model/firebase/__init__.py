a22cb9abe7710d5c1541b3a63a78a62edff43f53
Theodor Angergård <portalprogramming@gmail.com>
2019-05-17 12:51:19 +0200

    Continues work with trying to get storage to work

--- app/model/firebase/__init__.py
+++ app/model/firebase/__init__.py
@@ -7,6 +7,7 @@ credential_store = JsonStore('credentials.json')
 if credential_store.exists('tokens'):
     try:
         Auth.sign_in_with_tokens(**credential_store.get('tokens'))
-    except:
-        print("Deleting credentials.json")
-        credential_store.delete('tokens')
+    except Exception as error:
+        print(error)
+        print("Deleting credentials.json since it may be corrupted")
+        # credential_store.delete('tokens')
