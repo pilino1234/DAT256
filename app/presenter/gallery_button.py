baf895d9de4395cfd4a632ea63deab95e4ca2cbe
Elias Sundqvist <elias@sundqvist.com>
2019-05-18 13:46:13 +0200

    Monkey patch file selector so that history is not erased

--- app/presenter/gallery_button.py
+++ app/presenter/gallery_button.py
@@ -1,3 +1,5 @@
+import os
+
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.modalview import ModalView
@@ -29,6 +31,19 @@ class GalleryButton(MDFloatingActionButton):
             self.manager = ModalView(size_hint=(1, 1), auto_dismiss=False)
             self.file_manager = MDFileManager(exit_manager=self.exit_manager,
                                               select_path=self.select_path)
+
+            # Monkey patch file selection so that history is not erased
+            def select_dir_or_file(path):
+                """Called by tap on the name of the directory or file."""
+                if os.path.isfile(path):
+                    self.file_manager.select_path(path)
+                    return
+
+                self.file_manager.current_path = path
+                self.file_manager.show(path)
+
+            self.file_manager.select_dir_or_file = select_dir_or_file
+
             self.manager.add_widget(self.file_manager)
             self.file_manager.show('/')  # output manager to the screen
         self.manager_open = True
