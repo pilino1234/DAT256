c6409917a74eb27d464d9e6e8939ab16248e7bdd
pilino1234 <pilino1234@zoho.eu>
2019-05-19 23:56:27 +0200

    Fix signing in with saved credentials

--- app/presenter/screen/auth_screen.py
+++ app/presenter/screen/auth_screen.py
@@ -1,10 +1,30 @@
+from kivy.app import App
 from kivy.lang import Builder
+from kivy.storage.jsonstore import JsonStore
 from kivy.uix.screenmanager import Screen
+
+from model.firebase.auth import Auth
 
 Builder.load_file("view/screens/auth_screen.kv")
 
 
 class AuthScreen(Screen):
     """The screen containing all authentication related views."""
+
+    def __init__(self, **kwargs):
+        super(AuthScreen, self).__init__(**kwargs)
+
+        credential_store = JsonStore('credentials.json')
+
+        if credential_store.exists('tokens'):
+            try:
+                Auth.sign_in_with_tokens(**credential_store.get('tokens'))
+
+                app = App.get_running_app()
+                app.is_authenticated = True
+            except Exception as error:
+                print(error)
+                print("Deleting credentials.json since it may be corrupted")
+                credential_store.delete('tokens')
 
     pass
