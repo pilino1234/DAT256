304b23a9901229952696bacc177edb801f76e5a1
pilino1234 <pilino1234@zoho.eu>
2019-05-19 23:34:37 +0200

    Try to bring some sense back into the world

--- app/model/user_me_getter.py
+++ app/model/user_me_getter.py
@@ -3,24 +3,26 @@ from typing import Text, Optional
 
 from model.user import User
 from model.firebase.firestore import Firestore
+from model.user_getter import UserGetter
 
 
 class UserMeGetter:
 
-    _user_id: Text = None
-    user: User = User()
+    _user_id: Text = ''
+    user: User = None
 
     @staticmethod
-    def set_me(user_id: Text):
+    def set_me(new_user_id: Text):
         if UserMeGetter._user_id is not None:
             Firestore.unsubscribe("users/" + UserMeGetter._user_id)
 
-        UserMeGetter._user_id = user_id
-        Firestore.subscribe_document("users", user_id, UserMeGetter._on_snapshot_user)
+        UserMeGetter._user_id = new_user_id
+        Firestore.subscribe_document("users", new_user_id, UserMeGetter._on_snapshot_user)
+        UserMeGetter.user = UserGetter.get_by_id(new_user_id)
 
     @staticmethod
     def _on_snapshot_user(document_snapshot, _, __):
+        if UserMeGetter.user is None:
+            return
         document_dict = document_snapshot[0].to_dict()
         UserMeGetter.user.update(**document_dict)
-
-        print(UserMeGetter.user)
