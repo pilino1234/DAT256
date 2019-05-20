9e12990afc3e0cff3eecac029eeacb1a66071ad5
Elias Sundqvist <elias@sundqvist.com>
2019-05-20 09:44:38 +0200

    Add minified user class

---
+++ app/model/minified_user.py
@@ -0,0 +1,33 @@
+from typing import List
+
+from model.delivery_request import DeliveryRequest
+
+
+class MinifiedUser:
+    """Represents a user's account."""
+
+    def __init__(self, name: str, mail: str, phonenumber: str, uid: str):
+        self.name = name
+        self.mail = mail
+        self.phonenumber = phonenumber
+        self.uid = uid
+
+    def update(self, **kwargs):
+        if 'name' in kwargs:
+            self.name = kwargs['name']
+        if 'mail' in kwargs:
+            self.mail = kwargs['mail']
+        if 'phonenumber' in kwargs:
+            self.phonenumber = kwargs['phonenumber']
+        if 'uid' in kwargs:
+            self.uid = kwargs['uid']
+
+    def __eq__(self, other):
+        """Checks equality between users using their mail."""
+        return isinstance(other, MinifiedUser) and self.mail == other.mail
+
+    def __str__(self):
+        """Format a user for printing"""
+        return "User: {name}, {mail}, {phonenumber}".format(
+            name=self.name, mail=self.mail, phonenumber=self.phonenumber
+        )
