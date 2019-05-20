e188e6adcb596db242085f16f93fa62ebf714ce2
Theodor Angergård <portalprogramming@gmail.com>
2019-05-20 07:50:32 +0200

    Alot of progress on firebase functions

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,122 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.updateMinifiedUsers = functions.firestore
+    .document('users/{userId}')
+    .onUpdate(change => {
+        const newValue = change.after.data();
+
+        const uid = change.after.id;
+
+        const name = newValue.name;
+        const mail = newValue.mail;
+        const phonenumber = newValue.phonenumber;
+
+        const newMinifiedUser = {
+            name,
+            mail,
+            phonenumber,
+            uid
+        };
+
+        updateMinifiedUsers(newMinifiedUser);
+
+        updateMinifiedUsersPackages(
+            change.after.ref,
+            newMinifiedUser
+        );
+
+        updateMinifiedUsersDeliveres(
+            change.after.ref,
+            newMinifiedUser
+        );
+
+        return 0;
+    });
+
+exports.createMinifiedUsers = functions.firestore
+    .document('users/{userId}')
+    .onCreate(snap => {
+        const newValue = snap.data();
+
+        const uid = snap.id;
+        const name = newValue.name;
+        const mail = newValue.mail;
+        const phonenumber = newValue.phonenumber;
+
+        const newMinifiedUser = {
+            name,
+            mail,
+            phonenumber,
+            uid
+        };
+
+        updateMinifiedUsers(newMinifiedUser);
+
+        return 0;
+
+    });
+
+exports.createPackages = functions.firestore
+    .document('packages/{packageId}')
+    .onCreate(snap => {
+        updateUsersPackages(snap.data(), snap.id);
+        return 0;
+    });
+
+exports.updatePackages = functions.firestore
+    .document('packages/{packageId}')
+    .onUpdate(change => {
+        updateUsersPackages(change.after.data(), change.after.id);
+        return 0;
+    });
+
+function updateUsersPackages(data, packageId){
+    const ownerMinifiedUser = data.owner;
+    const assistantMinifiedUser = data.assistant;
+
+    updateUserPackage(ownerMinifiedUser.uid, packageId, data);
+    if(assistantMinifiedUser !== undefined && assistantMinifiedUser.uid !== null){
+        updateUserPackage(assistantMinifiedUser.uid, packageId, data);
+    }
+}
+
+function updateUserPackage(userId, packageId, packageData){
+    admin.firestore().collection('users').doc(userId).collection('packages').doc(packageId).set(packageData);
+}
+
+function updateMinifiedUsers(user){
+    admin.firestore().collection('minified-users').doc(user.uid).set({
+        name: user.name,
+        mail: user.mail,
+        phonenumber: user.phonenumber,
+        uid: user.uid
+    });
+}
+
+function updateMinifiedUsersPackages(ref, data) {
+    ref.collection('packages').get().then(querySnapshot => {
+        querySnapshot.forEach(documentSnapshot => {
+            console.log("Updating package in user");
+            console.log(documentSnapshot.ref.path);
+            console.log(data);
+            documentSnapshot.ref.update({owner: data});
+        });
+        return 0;
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+function updateMinifiedUsersDeliveres(ref, data) {
+    ref.collection('deliveres').get().then(querySnapshot => {
+        querySnapshot.forEach(documentSnapshot => {
+            documentSnapshot.ref.update({assistant: data});
+        });
+        return 0;
+    }).catch(error => {
+        console.log(error);
+    });
+}
