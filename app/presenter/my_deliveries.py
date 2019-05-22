27b001eab562642bda3ae6125757d75326458d5a
pilino1234 <pilino1234@zoho.eu>
2019-05-22 11:04:49 +0200

    Fix submitting packages and unify uses of dict vs Location when creating DeliveryRequests

--- app/presenter/my_deliveries.py
+++ app/presenter/my_deliveries.py
@@ -22,7 +22,8 @@ class MyDeliveries(BoxLayout):
         """Initializes the delivery list"""
         super(MyDeliveries, self).__init__(**kwargs)
         Clock.schedule_once(lambda dt: self._update_content())
-        Firestore.subscribe("users/" + UserMeGetter._user_id + "/deliveries", lambda *_: self._update_content())
+        Firestore.subscribe(u'users/{}/deliveries'.format(
+            UserMeGetter.user), lambda *_: self._update_content())
 
     def _update_content(self):
         """Fetch all deliveries the current owner has accepted"""
