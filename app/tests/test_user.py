25227343fcc33d5c0f1f2e82fc2e94b368629e22
Theodor Angergård <portalprogramming@gmail.com>
2019-05-22 15:18:27 +0200

    Resolves yapf styling

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -8,79 +8,109 @@ from model.minified_user import MinifiedUser
 
 class UserTest(unittest.TestCase):
     def test_equal(self):
-        u1 = User(
-            "", "User1", "email@example.com", "1234567890", "", balance=0)
+        u1 = User("",
+                  "User1",
+                  "email@example.com",
+                  "1234567890",
+                  "",
+                  balance=0)
 
-        u2 = User(
-            "",
-            "User2",
-            "email@example.com",
-            "0987654321",
-            "avatar_binary_string_stuff",
-            balance=10)
+        u2 = User("",
+                  "User2",
+                  "email@example.com",
+                  "0987654321",
+                  "avatar_binary_string_stuff",
+                  balance=10)
 
         self.assertTrue(u1 == u2)
         self.assertTrue(u2 == u1)
 
     def test_not_equal(self):
 
-        u1 = User(
-            "", "User1", "email@example.com", "1234567890", "", balance=0)
+        u1 = User("",
+                  "User1",
+                  "email@example.com",
+                  "1234567890",
+                  "",
+                  balance=0)
 
-        u2 = User(
-            "",
-            "User1",
-            "different_email@example.com",
-            "1234567890",
-            "",
-            balance=0)
+        u2 = User("",
+                  "User1",
+                  "different_email@example.com",
+                  "1234567890",
+                  "",
+                  balance=0)
 
         self.assertFalse(u1 == u2)
         self.assertFalse(u2 == u1)
 
     def test_deposit(self):
-        user = User(
-            "", "User1", "email@example.com", "1234567890", "", balance=0)
+        user = User("",
+                    "User1",
+                    "email@example.com",
+                    "1234567890",
+                    "",
+                    balance=0)
 
         self.assertEqual(user.balance, 0)
         user.deposit(100)
         self.assertEqual(user.balance, 100)
 
     def test_deposit_negative(self):
-        user = User(
-            "", "User1", "email@example.com", "1234567890", "", balance=50)
+        user = User("",
+                    "User1",
+                    "email@example.com",
+                    "1234567890",
+                    "",
+                    balance=50)
 
         self.assertEqual(user.balance, 50)
         user.deposit(-10)
         self.assertEqual(user.balance, 50)
 
     def test_withdraw(self):
-        user = User(
-            "", "User1", "email@example.com", "1234567890", "", balance=100)
+        user = User("",
+                    "User1",
+                    "email@example.com",
+                    "1234567890",
+                    "",
+                    balance=100)
 
         self.assertEqual(user.balance, 100)
         user.withdraw(50)
         self.assertEqual(user.balance, 50)
 
     def test_withdraw_negative(self):
-        user = User(
-            "", "User1", "email@example.com", "1234567890", "", balance=100)
+        user = User("",
+                    "User1",
+                    "email@example.com",
+                    "1234567890",
+                    "",
+                    balance=100)
 
         self.assertEqual(user.balance, 100)
         user.withdraw(-50)
         self.assertEqual(user.balance, 100)
 
     def test_withdraw_more_than_balance(self):
-        user = User(
-            "", "User1", "email@example.com", "1234567890", "", balance=100)
+        user = User("",
+                    "User1",
+                    "email@example.com",
+                    "1234567890",
+                    "",
+                    balance=100)
 
         self.assertEqual(user.balance, 100)
         user.withdraw(1000000)
         self.assertEqual(user.balance, 100)
 
     def test_locking_money(self):
-        user = User(
-            "", "User1", "email@example.com", "1234567890", "", balance=100)
+        user = User("",
+                    "User1",
+                    "email@example.com",
+                    "1234567890",
+                    "",
+                    balance=100)
 
         request = DeliveryRequest(
             "id",
